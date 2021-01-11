dbb050a21bc749d3214ac8e8b2905adb4b79239c
Kaitlyn Hova <kaitlynhova@gmail.com>
2021-01-10 19:44:44 -0700

    adding generic theme

---
+++ src/Theme/ThemeLight.ts
@@ -0,0 +1,29 @@
+const spacing = [0, 4, 8, 12, 16, 20, 24, 28, 32];
+
+const typography = {
+  fontFamily: "Inter,sans-serif",
+  p: {
+    fontSize: "1.2em",
+  },
+  h1: {
+    fontSize: "1.7em",
+  },
+  logo: {
+    fontSize: "1.3em",
+  },
+};
+
+export const ThemeLight = {
+  palette: {
+    background: "#F0F2F5",
+    onBackground: "#000000",
+    onPrimary: "#ffffff",
+    onSurface1: "#ffffff",
+    onSurface2: "#000000",
+    primary: "#000000",
+    surface1: "#000000",
+    surface2: "#ffffff",
+  },
+  spacing,
+  typography,
+};
